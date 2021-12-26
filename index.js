98d1128317cb775ac48ef8dfbf6866d924ce1b7e
ramikadaweh <ramikadaweh31@gmail.com>
2021-12-26 14:01:41 +0200

    step 5

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const test = {
 const time = {
     status:200, message:hour+':'+second
 };
-
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 
 app.get('/', (req, res) => {
@@ -37,6 +42,19 @@ app.get('/search', (req, res) => {
     res.send({
         status: 200, message: "ok", data: s 
     }) : res.status(500).json({ status: 500, error: true, message: "you have to provide a search" });
+})
+
+app.get('/movies/add', (req, res) => {
+    res.send()
+})
+app.get('/movies/get', (req, res) => {
+    res.send({status:200, data:movies})
+})
+app.get('/movies/edit', (req, res) => {
+    res.send()
+})
+app.get('/movies/delete', (req, res) => {
+    res.send()
 })
 
 app.listen(port, () => {
